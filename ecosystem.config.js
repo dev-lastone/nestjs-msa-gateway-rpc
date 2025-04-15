7387e036cff9c35193352025466797cb6049e4e1
jjong <akrTmwk@gmail.com>
2025-04-15 16:43:03 +0900

    chore: ecosystem.config.js 설정 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'gateway',
+      script: './dist/apps/gateway/main.js',
+    },
+    {
+      name: 'hello',
+      script: './dist/apps/hello/main.js',
+    },
+  ],
+};
