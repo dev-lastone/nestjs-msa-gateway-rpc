0de923a8608fca5612216cc3ab9f56941c03642a
jjong <akrTmwk@gmail.com>
2025-04-15 16:32:00 +0900

    feat: gateway hello 연결

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientProxyFactory } from '@nestjs/microservices';
+import { ConfigService } from './config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [GatewayController],
-  providers: [GatewayService],
+  providers: [
+    ConfigService,
+    {
+      provide: process.env.HELLO_SERVICE_NAME,
+      useFactory: (configService: ConfigService) => {
+        const options = configService.get('helloService');
+        return ClientProxyFactory.create(options);
+      },
+      inject: [ConfigService],
+    },
+    GatewayService,
+  ],
 })
 export class GatewayModule {}
